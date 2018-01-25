d8fd98ad67ca0d96f2106825805a994022882570
Anoop M <noop2016@gmail.com>
2018-01-25 20:00:32 +0000

    added only if to exec puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fix word press typo thats preventing it from running
 
 exec { 'modify wp-settings':
+  onlyif  => '/var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin',
 }
