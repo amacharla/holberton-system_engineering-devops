38cebd1a2a476b42dc55090451872416b396ee95
Anoop M <noop2016@gmail.com>
2018-01-31 02:32:31 +0000

    0 done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fix Nginx OpenFile limit
+# saw the limts at /etc/procs/<PID of NGINX root>/limits
+
+exec { 'Increase Ulimit for openfile':
+  onlyif  => 'test -f /etc/default/nginx',
+  command => 'sed "s/ULIMIT=\"-n 15/ULIMIT=\"-n 8000/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+exec { 'Restart Nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Modify OS configuration 
+# saw the limts at /etc/procs/<PID of NGINX root>/limits
+
+exec { 'Increase Ulimit for openfile':
+  onlyif  => 'test -f /etc/default/nginx',
+  command => 'sed "s/ULIMIT=\"-n 15/ULIMIT=\"-n 8000/" /etc/security/limits.conf', 
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+exec { 'Restart Nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
